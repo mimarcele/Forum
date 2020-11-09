d78a362bc33e6fdbaf5d173520363b59dd5caf81
mimarcele <66285986+mimarcele@users.noreply.github.com>
2020-11-09 17:29:04 -0300

    Update HelloController.java

--- src/main/java/com/alura/controller/HelloController.java
+++ src/main/java/com/alura/controller/HelloController.java
@@ -1,16 +1,1 @@
-package com.alura.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-public class HelloController {
-
-    @RequestMapping("/")
-    @ResponseBody
-    public String hello() {
-        return "Bem vindo a plataforma Mimi";
-    }
-
-}
